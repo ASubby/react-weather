73ab0df8f252bd9bb5c54cf42c1486965a71571d
ASubby <104353408+ASubby@users.noreply.github.com>
2022-08-11 10:25:36 +0100

    Updated undex.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Weather from "./Weather";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
